71ba9acf62b19020b118e23ebac0e17df9bbede1
Roque Ayala Angel David <ingRoque98@gmail.com>
2021-06-16 10:23:29 -0500

    Reducir el numero de peticiones http

--- src/app/pages/shared/barra-busqueda/barra-busqueda.component.ts
+++ src/app/pages/shared/barra-busqueda/barra-busqueda.component.ts
@@ -1,38 +1,54 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-barra-busqueda',
   templateUrl: './barra-busqueda.component.html',
-  styleUrls: ['./barra-busqueda.component.css']
+  styleUrls: ['./barra-busqueda.component.css'],
 })
 export class BarraBusquedaComponent implements OnInit {
-
   @Output() contenidoInputEmmiter = new EventEmitter<string>();
-  contenidoInput='';
+  contenidoInput = '';
   scanerDisponible = false;
+  results$: Observable<any>;
+  subject = new Subject();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.results$ = this.subject.pipe(
+      debounceTime(1000)
+    );
+
+    this.results$.subscribe(() => {
+      this.emitirCambio()
+    })
   }
 
-  emitirCambio(){
+  emitirCambio() {
+    console.log('hola');
     
-    this.contenidoInputEmmiter.emit(this.contenidoInput)
+    this.contenidoInputEmmiter.emit(this.contenidoInput);
   }
 
-  iniciarScanner(){
+  iniciarScanner() {
     this.scanerDisponible = true;
   }
 
-  addItem($event){
+  addItem($event) {
     this.contenidoInput = $event;
     this.scanerDisponible = false;
     this.emitirCambio();
   }
 
-  clearInput(){
+  clearInput() {
     this.contenidoInput = '';
     this.emitirCambio;
+  }
+
+  search() {
+    // emits the `searchText` into the stream. This will cause the operators in its pipe function (defined in the ngOnInit method) to be run. `debounceTime` runs and then `map`. If the time interval of 1 sec in debounceTime hasn’t elapsed, map will not be called, thereby saving the server from being called.
+    this.subject.next(this.contenidoInput);
   }
 }
